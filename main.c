de8ca1171fedec251168ecfde961ed88cca7ee40
xp4w <132715432+xp4w@users.noreply.github.com>
2024-02-22 15:55:18 +0000

    Include shaders.h in main.c

--- main.c
+++ main.c
@@ -4,19 +4,7 @@
 #include <stdio.h>
 
 #include "defines.h"
-
-const char* vertexShaderSource = "#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"void main()\n"
-	"{\n"
-	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-	"}\0";
-const char* fragmentShaderSource = "#version 330 core\n"
-	"out vec4 FragColor;\n"
-	"void main()\n"
-	"{\n"
-	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-	"}\0";
+#include "shaders.h"
 
 /* Whenever the window size is changed this callback function executes */
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
