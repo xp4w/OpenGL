aaf0878db683e6cdebc4632e48e42b3ba6d3aa78
xp4w <benzilla09.xbl@gmail.com>
2024-02-22 18:45:04 -0600

    now we have... SQUARE!

--- main.c
+++ main.c
@@ -89,19 +89,28 @@ int main()
 	/* Vertex data */
 	float vertices[] = {
 		/* X      Y     Z */
-		-0.5f, -0.5f, 0.0f, /* left  */
-		 0.5f, -0.5f, 0.0f, /* right */
-		 0.0f,  0.5f, 0.0f  /* top   */
+		 0.5f,  0.5f, 0.0f, /* Top right    */
+		 0.5f, -0.5f, 0.0f, /* Bottom right */
+		-0.5f, -0.5f, 0.0f, /* Bottom left  */
+		-0.5f,  0.5f, 0.0f, /* Top left     */
+	};
+	unsigned int indices[] = {
+		0, 1, 3, /* First triangle  */
+		1, 2, 3, /* Second triangle */
 	};
 
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -109,6 +118,8 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glBindVertexArray(0);
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	/* Render loop */
 	while (!glfwWindowShouldClose(window)) {
@@ -118,10 +129,10 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		/* Draw triangle :D */
+		/* Draw square ;) */
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		/* Check and call events and swap buffers */
 		glfwSwapBuffers(window);
