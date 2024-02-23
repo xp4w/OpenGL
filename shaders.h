00b8b7eecc65fc387ce4754905435a5847809fca
xp4w <benzilla09.xbl@gmail.com>
2024-02-22 19:07:33 -0600

    forgor to commit this

--- shaders.h
+++ shaders.h
@@ -1,14 +1,16 @@
 #pragma once
 
 const char* vertexShaderSource = "#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"void main()\n"
-	"{\n"
-	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+  "layout (location = 0) in vec3 aPos;\n"
+  "void main()\n"
+  "{\n"
+  "  gl_Position = vec4(aPos, 1.0);\n"
 	"}\0";
+
 const char* fragmentShaderSource = "#version 330 core\n"
-	"out vec4 FragColor;\n"
-	"void main()\n"
-	"{\n"
-	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-	"}\0";
+  "out vec4 FragColor;\n"
+  "uniform vec4 ourColor;\n"
+  "void main()\n"
+  "{\n"
+  "  FragColor = ourColor;\n"
+  "}\n\0";
